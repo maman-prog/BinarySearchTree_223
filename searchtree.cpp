4156f8bcffa885f13244f95c269df7e8ccce6da4
Maman <ingratarmanrizky19@gmail.com>
2026-06-09 11:17:34 +0700

    void postorder

--- searchtree.cpp
+++ searchtree.cpp
@@ -91,4 +91,36 @@ public:
         }
     }
 
-    
+    void preorder(node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
+    void postorder(node *ptr)
+    {
+        if (root == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if(ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
+
